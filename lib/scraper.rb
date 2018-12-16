3fa846711cecf43fec43863681cb7952f7752090
Elise Phan <aliceconnic@gmail.com>
2018-12-16 07:34:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,8 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     doc.css(".roster-cards-container").each do |card|
-      card.css(".student-card a")
+      card.css(".student-card a").each do |student|
+        
    
  
    
