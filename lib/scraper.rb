20777d94a77b42f94fa7821c72866be1b4c0aa50
Elise Phan <aliceconnic@gmail.com>
2018-12-16 07:24:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
-    doc.css("div.roster-cards-container").each do |card|
+    doc.css(".roster-cards-container").each do |card|
       card.css(".student-card a")
      binding.pry
  
