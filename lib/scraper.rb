90f0015caeb0d799b05b56a227859aef293ab3ee
Elise Phan <aliceconnic@gmail.com>
2018-12-16 05:41:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
      doc = Nokogiri::HTML(open("http://159.89.134.39:47526/fixtures/student-site/"))
-     name = doc.search("h4#student-name").text
+     name = doc.css("h4#student-name").text
      name.each do |student|
        puts student.css("h4").text
      end
