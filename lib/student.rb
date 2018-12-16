817916bd4e07f0d08064de673cd0212d241d344f
Elise Phan <aliceconnic@gmail.com>
2018-12-16 10:06:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,9 @@ class Student
   end
 
 
-
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
 
   def self.create_from_collection(students_array)
     
