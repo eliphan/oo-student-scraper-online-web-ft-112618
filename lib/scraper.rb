f7fd78222b178d7c52d953af1a4fe3836d89ae4f
Elise Phan <aliceconnic@gmail.com>
2018-12-16 05:31:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 require 'pry'
 
 class Scraper
-  doc = Nokogiri::HTML(open(url))
+  doc = Nokogiri::HTML(open(http://159.89.134.39:47526/fixtures/student-site/)
 
   def self.scrape_index_page(index_url)
     
