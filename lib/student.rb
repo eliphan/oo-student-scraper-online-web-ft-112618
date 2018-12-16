6c7dc07d0d7abf227fc6047ef1b7be8fa096de27
Elise Phan <aliceconnic@gmail.com>
2018-12-16 10:15:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    new_student = student.new(student_hash)
+    new_student = 
     students_array.map do |student| self.send
   end
 
